c45a7687b1db167c22841e13cf8000c86371f81d
Asmaa Adel <74569407+asmaaadel41@users.noreply.github.com>
2022-06-26 01:15:52 -0800

    Adding styles for about and contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
         'bgcolor':'#EFEFEF'
         
       },
+    },
+        letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
     }
   },
 }
